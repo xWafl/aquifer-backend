b1f323c77b1f1884475d534e95bee059b96c088a
xWafl <73sampleperson@gmail.com>
2020-03-16 19:47:29 -0400

    :fire: Removed unnecessary code in my knexfile

--- knexfile.js
+++ knexfile.js
@@ -7,15 +7,6 @@ module.exports = {
 
     development: {
         client: 'pg',
-        // connection: {
-        //     host : process.env.HOST,
-        //     user : process.env.USER,
-        //     password : process.env.PASSWORD,
-        //     database : process.env.DATABASE,
-        //     port: process.env.PORT,
-        //     ssl: true,
-        //     charset: 'utf8'
-        // },
         connection: process.env.DATABASE_URL,
         ssl: {
             rejectUnauthorized: false
@@ -28,33 +19,8 @@ module.exports = {
         }
     },
 
-    // staging: {
-    //     client: 'postgresql',
-    //     connection: {
-    //         database: 'my_db',
-    //         user: 'username',
-    //         password: 'password'
-    //     },
-    //     pool: {
-    //         min: 2,
-    //         max: 10
-    //     },
-    //     migrations: {
-    //         tableName: 'knex_migrations'
-    //     }
-    // },
-
     production: {
         client: 'pg',
-        // connection: {
-        //     host : process.env.HOST,
-        //     user : process.env.USER,
-        //     password : process.env.PASSWORD,
-        //     database : process.env.DATABASE,
-        //     port: process.env.PORT,
-        //     ssl: true,
-        //     charset: 'utf8'
-        // },
         connection: process.env.DATABASE_URL,
         ssl: {
             rejectUnauthorized: false
