24166cfe56520e890c54c64c5ca2c00c4bd67e64
xWafl <73sampleperson@gmail.com>
2020-03-16 15:06:57 -0400

    :recycle: fixed a typo

--- init.ts
+++ init.ts
@@ -69,7 +69,7 @@ const getHighestId = async (): Promise<number> => {
         id: number
     }
     const ids: Array<idRet> = await knex("messages").select("id");
-    const arrIds = ids.map(({ id }) => ld);
+    const arrIds = ids.map(({ id }) => id);
     if (arrIds.length === 0) {
         return 0;
     } else {
