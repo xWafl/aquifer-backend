dbb6b4c6ebba12bfe104748591b167cb07fc1dd4
xWafl <73sampleperson@gmail.com>
2020-03-18 12:14:04 -0400

    :sparkles: Added session keys as well as a login/create/logout system

--- index.ts
+++ index.ts
@@ -18,7 +18,6 @@ import {init, getHighestId} from './init';
 import {editMessage} from "./wss";
 
 let messages: Array<Message> = [];
-let highestUserId = 0;
 let highestChannelId = 0;
 let users = {};
 let channels = {};
@@ -28,6 +27,11 @@ const sendToClients = (category, data) => {
     wss.clients.forEach(function each(ws) {
         ws.send(JSON.stringify([category, data]));
     })
+};
+
+const checkAuth = async (seshkey: string) => {
+    const matchingSK = await knex("accounts").where({seshkey: seshkey}).select("*");
+    return matchingSK.length > 0;
 };
 
 const app = require("./auth");
@@ -48,10 +52,11 @@ process.on('uncaughtException', function (err) {
 const wss = new Server({server});
 
 wss.on('connection', function connection(ws) {
+    console.log(`new connection`);
     ws.isAlive = true;
     ws.on('message', async (data) => {
         if (data !== '{"kind":"ping"}') {
-            const [category, message] = JSON.parse(data);
+            const [category, seshkey, message] = JSON.parse(data);
             if (category === "message") {
                 const msgInfo = message;
                 msgInfo.utcTime = moment().valueOf();
@@ -96,57 +101,106 @@ wss.on('connection', function connection(ws) {
                 sendToClients("deleteMessage", selectedMessage);
             }
             if (category === "queryMessages") {
+                console.log(`messages queried`);
                 sendToClients("messageList", messages);
             }
             if (category === "queryChannels") {
                 sendToClients("channelList", channels);
             }
             if (category === "newUser") {
-                const rows = await knex("serverid").catch(e => {throw e});
-                const sNum = rows[0].snum;
-                const theUser: User = {
-                    username: message.username,
-                    usernum: message.userNum,
-                    currentChannel: 1,
-                    id: ++highestUserId,
-                    messages: message.messages
-                };
-                const queryDetails = {
-                    username: theUser.username,
-                    usernum: theUser.usernum,
-                    currentchannel: theUser.currentChannel,
-                    id: theUser.id,
-                    snum: sNum
-                };
-                knex("users")
-                    .insert(queryDetails)
-                    .catch((err) => {
-                        throw err;
-                    });
-                ws.userDetails = theUser;
-                const arrayClients = Array.from(wss.clients);
-                // @ts-ignore
-                const arrayUsers: Array<User> = Array.from(arrayClients, x => x.userDetails).filter(l => l != null);
-                users = arrayUsers.reduce((acc, elem) => {acc[elem.id] = elem;return acc;}, {});
-                ws.send(JSON.stringify(["bestowId", theUser.id]));
-                sendToClients("newUser", users);
+                // const rows = await knex("serverid").catch(e => {throw e});
+                // const sNum = rows[0].snum;
+                // const theUser: User = {
+                //     username: message.username,
+                //     usernum: message.userNum,
+                //     currentChannel: 1,
+                //     id: ++highestUserId,
+                //     messages: message.messages
+                // };
+                // const queryDetails = {
+                //     username: theUser.username,
+                //     usernum: theUser.usernum,
+                //     currentchannel: theUser.currentChannel,
+                //     id: theUser.id,
+                //     snum: sNum
+                // };
+                // knex("users")
+                //     .insert(queryDetails)
+                //     .catch((err) => {
+                //         throw err;
+                //     });
+                // ws.userDetails = theUser;
+                // const arrayClients = Array.from(wss.clients);
+                // // @ts-ignore
+                // const arrayUsers: Array<User> = Array.from(arrayClients, x => x.userDetails).filter(l => l != null);
+                // users = arrayUsers.reduce((acc, elem) => {acc[elem.id] = elem;return acc;}, {});
+                // ws.send(JSON.stringify(["bestowId", theUser.id]));
+                // sendToClients("newUser", users);
+                const isAuth = checkAuth(seshkey);
+                if (isAuth) {
+                    await knex("accounts")
+                        .where({seshkey: seshkey})
+                        .update({status: "online"})
+                        .catch(e => {
+                            throw e;
+                        });
+                    const account = await knex("accounts")
+                        .where({seshkey: seshkey})
+                        .select("*")
+                        .catch(e => {
+                            throw e;
+                        });
+                    const theUser: User = {
+                        username: account[0].username,
+                        usernum: account[0].usernum,
+                        currentChannel: account[0].currentchannel,
+                        id: account[0].id,
+                        messages: account[0].messages
+                    };
+                    users[theUser.id] = theUser;
+                    sendToClients("newUser", theUser);
+                }
             }
             if (category === "loseUser") {
-                delete users[message.id];
-                delete users[ws.userId];
-                knex("users")
-                    .where({id: message.id})
-                    .del()
-                    .catch((err) => {
-                        throw err;
-                    });
-                knex("messages")
-                    .where({userid: message.id})
-                    .update({userid: 0})
-                    .catch((err) => {
-                        throw err;
-                    });
-                sendToClients("loseUser", users);
+                // delete users[message.id];
+                // delete users[ws.userId];
+                // knex("users")
+                //     .where({id: message.id})
+                //     .del()
+                //     .catch((err) => {
+                //         throw err;
+                //     });
+                // knex("messages")
+                //     .where({userid: message.id})
+                //     .update({userid: 0})
+                //     .catch((err) => {
+                //         throw err;
+                //     });
+                // sendToClients("loseUser", users);
+                const isAuth = checkAuth(seshkey);
+                if (isAuth) {
+                    const account = await knex("accounts")
+                        .where({seshkey: seshkey})
+                        .select("*")
+                        .catch(e => {
+                            throw e;
+                        });
+                    const theUser: User = {
+                        username: account[0].username,
+                        usernum: account[0].usernum,
+                        currentChannel: account[0].currentchannel,
+                        id: account[0].id,
+                        messages: account[0].messages
+                    };
+                    delete users[theUser.id];
+                    sendToClients("loseUser", theUser.id);
+                    knex("accounts")
+                        .where({seshkey: seshkey})
+                        .del()
+                        .catch(e => {
+                            throw e;
+                        })
+                }
             }
             if (category === "newChannel") {
                 const channelId = ++highestChannelId;
